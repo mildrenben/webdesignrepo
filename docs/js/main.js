c3362e9de2bed4ea99cc5e88cba2af408f7acee3
Benjamin Mildren <Ben@Benjamins-MBP.home>
2016-10-25 20:22:45 +0100

    Fixed anchor links on <760px wide not working

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 var unseen;
 var recentType;
-var path = window.location.pathname; 
+var path = window.location.pathname;
 if (path === '/') {
   recentType = 'wdr_recent_webdesign';
 } else if (path === '/frontend.html') {
@@ -339,6 +339,13 @@ function SideNavTabs () {
 };
 
 function SmoothScrollAnchor() {
+  var Hamburger = document.getElementsByClassName('TopBar_Hamburger')[0];
+  var SideNav = document.getElementsByClassName('SideNav')[0];
+  var SideNavBackdrop = document.getElementsByClassName('SideNav_Backdrop')[0];
+  function toggleSideNav() {
+    SideNav.classList.toggle('SideNav--visible');
+    SideNavBackdrop.classList.toggle('SideNav_Backdrop--visible');
+  }
   // Smooth Scrolling Anchor Links
   $('a[href^="#"]').click(function(e) {
       e.preventDefault();
