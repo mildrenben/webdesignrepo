f5fcc0b8a4270b95256177277a3e66835fe2992d
Benjamin Mildren <Ben@Benjamins-MBP.home>
2016-11-01 09:55:49 +0000

    Fixed sidebar bug at tablet size

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 var unseen;
 var recentType;
-var path = window.location.pathname;
+var path = window.location.pathname; 
 if (path === '/') {
   recentType = 'wdr_recent_webdesign';
 } else if (path === '/frontend.html') {
@@ -339,27 +339,25 @@ function SideNavTabs () {
 };
 
 function SmoothScrollAnchor() {
-  var Hamburger = document.getElementsByClassName('TopBar_Hamburger')[0];
-  var SideNav = document.getElementsByClassName('SideNav')[0];
-  var SideNavBackdrop = document.getElementsByClassName('SideNav_Backdrop')[0];
-  function toggleSideNav() {
-    SideNav.classList.toggle('SideNav--visible');
-    SideNavBackdrop.classList.toggle('SideNav_Backdrop--visible');
-  }
   // Smooth Scrolling Anchor Links
   $('a[href^="#"]').click(function(e) {
       e.preventDefault();
       // calculate destination place
       var scrollDest = $(this.hash)[0].offsetTop - 70;
 
-      if($(window).width() < 600) {
-        toggleSideNav();
+      if($(window).width() < 760) {
+          toggleSideNav();
       }
 
       // go to destination
       $('.Links').animate({
           scrollTop: scrollDest
       }, 500, 'swing');
+      var currentPath = window.location.href;
+      if (currentPath.includes('#')) {
+        currentPath = currentPath.substr(0, currentPath.indexOf('#'));
+      }
+      history.pushState({}, '', currentPath + $(this).attr("href"));
   });
 };
 
